ecc512f466c102790e42d3f8959f7c86ba945dc0
David Linke <davidlinke@gmail.com>
2022-01-31 07:58:57 -0500

    further refinement of select

--- src/components/ptc-select/ptc-select.js
+++ src/components/ptc-select/ptc-select.js
@@ -14,12 +14,27 @@ class ParticleSelect extends Ptc {
     constructor() {
         super();
 
+        // state
+        this._isOpen = false;
+
         // initalize all the card properties
         this._initProps([{
             name: 'value',
             defaultValue: null,
             type: 'string'
+        },
+        {
+            name: 'open',
+            defaultValue: false,
+            type: 'boolean'
         }]);
+
+        // let's deal with the focus behavior
+        // if it doesnt have a tabindex, set it to 0 so it can focus
+        if (!this.hasAttribute('tabindex') && this.tabIndex === -1) {
+            this.tabIndex = 0;
+        }
+        this.addEventListener('focus', this._handleFocus)
 
         // attach shadow dom
         this._shadow = this.attachShadow({ mode: 'open' });
@@ -28,24 +43,81 @@ class ParticleSelect extends Ptc {
         const styleElement = this._setupStyleElement(styles)
         this._shadow.append(styleElement);
 
+        // create the clicker element
         const box = document.createElement('div');
         box.classList.add('box');
-        box.innerText = 'Select';
         this._shadow.append(box);
 
+        box.addEventListener('click', this.toggle.bind(this));
+
+        // create the output window
+        const boxOutput = document.createElement('div');
+        boxOutput.classList.add('box__output');
+        boxOutput.innerHTML = 'select';
+
+        box.append(boxOutput);
+
+        // create the chevron
+        const chevron = document.createElement('div');
+        chevron.classList.add('chevron');
+        chevron.innerHTML = `
+        <svg 
+            xmlns="http://www.w3.org/2000/svg" 
+            width="24" 
+            height="24" 
+            viewBox="0 0 24 24" 
+            fill="none" 
+            stroke="currentColor" 
+            stroke-width="2" 
+            stroke-linecap="round" 
+            stroke-linejoin="round" 
+            class="feather feather-chevron-down"
+        >
+            <polyline points="6 9 12 15 18 9"></polyline>
+        </svg>`;
+        box.append(chevron)
+
+        // create the dropdown
         const dropdown = document.createElement('div');
         dropdown.classList.add('dropdown');
-
+        // add the dropdown to the state so we can access it later
+        this._dropdown = dropdown;
+        if (this.open) {
+            dropdown.classList.add('dropdown--open');
+        }
+        // create the dropdown list
         const dropdownScroller = document.createElement('div');
         dropdownScroller.classList.add('dropdown__scroller');
 
         dropdown.appendChild(dropdownScroller);
-
-        // create the html
-        const slot = document.createElement('slot');
         dropdownScroller.append(slot);
 
+        // create the slot for the options
+        const slot = document.createElement('slot');
         this._shadow.append(dropdown);
+    }
+
+    _handleFocus() {
+        // let's start keystroke listening to emulate an actual select
+    }
+
+    toggle() {
+        if (this._isOpen) {
+            this.close();
+        } else {
+            this.open();
+        }
+
+    }
+
+    showOptions() {
+        this._isOpen = true;
+        this._dropdown.classList.add('visible');
+    }
+
+    hideOptions() {
+        this._isOpen = false;
+        this._dropdown.classList.remove('visible');
     }
 }
 
