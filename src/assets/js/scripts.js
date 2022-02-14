14ba63c8501bca0cdd74b6c6f7e60528194bf81b
David Linke <davidlinke@gmail.com>
2022-02-14 09:08:34 -0500

    Added a bit of organization

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,3 +1,21 @@
 import { LoadParticles } from '/components/index.js'
 
-LoadParticles(['ptc-button','ptc-card', 'ptc-content','ptc-checkbox','ptc-input','ptc-select','ptc-option']);
+LoadParticles(['ptc-button', 'ptc-card', 'ptc-content', 'ptc-checkbox', 'ptc-input', 'ptc-select', 'ptc-option']);
+
+(function () {
+    //button = document.querySelector('ptc-button');
+    document.querySelectorAll('ptc-checkbox').forEach((checkbox) => {
+        checkbox.addEventListener('blur', () => {
+            console.log('blur')
+        }, true)
+    })
+    const feather = feather || undefined;
+    if (feather) {
+        feather.replace({
+            'stroke-width': 2,
+            width: 22,
+            height: 22
+        });
+    }
+
+})();
